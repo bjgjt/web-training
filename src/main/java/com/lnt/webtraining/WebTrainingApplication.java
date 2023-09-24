c11a95360096a586dd3598096ccdadbcc82673f6
tuanln <tuanln@vnpt.vn>
2023-09-24 15:45:18 +0700

    first commit

---
+++ src/main/java/com/lnt/webtraining/WebTrainingApplication.java
@@ -0,0 +1,13 @@
+package com.lnt.webtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lnt/webtraining/WebTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lnt.webtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
